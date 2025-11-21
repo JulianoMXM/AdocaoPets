8ffcca1cf28a2a558c366f902d3482c435021f7e
JulianoMXM <juliano.massocalegal@gmail.com>
2025-11-21 15:28:36 -0300

    feat: Cria a validação dos dados do usuário

---
+++ backend/controllers/UserController.ts
@@ -0,0 +1,74 @@
+import { constants } from 'buffer'
+import { User } from '../models/User'
+import { Request, Response} from 'express'
+import bcrypt  from 'bcrypt'
+
+export class UserController {
+
+    static async register(req: Request, res: Response){
+        
+        const {name, email, password, confirmpassword, phone} = req.body
+
+        if(!name){
+            res.status(422).json({message: 'O nome é obrigatório.'})
+            return
+        }
+        if(!email){
+            res.status(422).json({message: 'O email é obrigatório.'})
+            return
+        }
+        if(!password){
+            res.status(422).json({message: 'A senha é obrigatória.'})
+            return
+        }
+        if(!confirmpassword){
+            res.status(422).json({message: 'A confirmação de senha é obrigatória.'})
+            return
+        }
+        if(!phone){
+            res.status(422).json({message: 'O telefone é obrigatório.'})
+            return
+        }
+
+        if(password !== confirmpassword){
+            res.status(422).json({message: 'As senhas digitadas são diferentes.'})
+            return
+        }
+
+        //Teste de usuário existente
+
+        const userExists = await User.findOne({email: email})
+
+        if(userExists){
+            res.status(422).json({message: 'Já existe um usuário com esse email.'})
+            return
+        }
+
+        //Criação de senha
+        const salt = await bcrypt.genSalt(12)
+        const passwordHash = await bcrypt.hash(password, salt)
+
+        //Criação de Usuário
+
+        const user = new User({
+            name,
+            email,
+            phone,
+            password: passwordHash
+        })
+
+        try {
+
+            const newUser = await user.save()
+            res.status(201).json(newUser)
+            return
+
+        } catch(error) {
+            if(error instanceof Error){
+                res.status(500).json({message: error.message})
+            }
+        }
+
+    }
+
+}
