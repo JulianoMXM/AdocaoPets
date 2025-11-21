8aebea33b9d39042e4df2701e6add92b8d40705a
JulianoMXM <juliano.massocalegal@gmail.com>
2025-11-20 21:25:20 -0300

    feat: Cria a interface de usuário

---
+++ backend/models/User.ts
@@ -0,0 +1,36 @@
+import {Schema, model} from 'mongoose'
+
+export interface IUser extends Document{
+
+    name: string,
+    email: string,
+    password: string,
+    phone: string,
+    createdAt: Date,
+    updatedAt: Date
+
+}
+
+const userSchema = new Schema<IUser>({
+
+        name: {
+            type: String, 
+            required: true
+        },
+        email: {
+            type: String, 
+            required: true, 
+            unique: true
+        },
+        password: {
+            type: String, 
+            required: true
+        },
+        phone: {type: String, 
+            required: true
+        }
+    }, 
+    { timestamps: true }
+)
+
+export const User = model<IUser>('User', userSchema)
