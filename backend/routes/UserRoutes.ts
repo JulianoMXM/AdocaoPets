56422d49e1c40d27d5fa9c1cef643c50445fee53
JulianoMXM <juliano.massocalegal@gmail.com>
2025-11-21 15:29:06 -0300

    feat: importa o registro de usuário na rota do usuário

--- backend/routes/UserRoutes.ts
+++ backend/routes/UserRoutes.ts
@@ -1,1 +1,7 @@
 import { UserController } from '../controllers/UserController'
+import { Router } from 'express'
+const router = Router()
+import type {Response, Request } from 'express'
+import {User, type IUser} from '../models/User.js'
+
+router.post('/register', UserController.register)
