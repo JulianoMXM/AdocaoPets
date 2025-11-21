34aad6f15d8e19f70c5ca51292cc82e3c40f0a9a
JulianoMXM <juliano.massocalegal@gmail.com>
2025-11-20 21:25:52 -0300

    refact: Ajusta o arquivo principal para usar o banco de dados no arquivo correto e já define para utilizar as rotas ainda não implementadas

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,7 @@
-import 'dotenv/config';
-import express from 'express';
-import mongoose from 'mongoose';
-const app = express();
+import 'dotenv/config'
+import express from 'express'
+import { main } from './db/conn'
+const app = express()
 
 //  Configuração de leitura de JSON
 
@@ -11,22 +11,17 @@ app.use(
     })
 );
 
-app.use(express.json());
+app.use(express.json())
 
 //  Porta utilizada
 
-const DB_USER = process.env.DB_USER
-const DB_PASSWORD = process.env.DB_PASSWORD
-
-mongoose
-    .connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@projetosunect.wtzr5ke.mongodb.net/`
-)
-    .then(() => {
-        console.log('Conectado');
-        app.listen(3000);
+main().then(() => {
+    app.listen(3000, () => {
+        console.log('Servidor Rodando')
     })
-    .catch((error) => {
-        console.error('Erro ao conectar ao banco:', error);
-    })
+})
     
 //  Rotas da API
+
+app.use('/users', UserRoutes)
+app.use('/pets', PetRoutes)
