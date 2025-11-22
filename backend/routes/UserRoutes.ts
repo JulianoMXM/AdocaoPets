7dcd24b5747f9633c4b08305009eede0cba7c16d
JulianoMXM <juliano.massocalegal@gmail.com>
2025-11-21 22:05:12 -0300

    feat: exporta o router da rota dos usuários e adiciona a rota de login

--- backend/routes/UserRoutes.ts
+++ backend/routes/UserRoutes.ts
@@ -1,7 +1,8 @@
 import { UserController } from '../controllers/UserController'
 import { Router } from 'express'
-const router = Router()
+export const router = Router()
 import type {Response, Request } from 'express'
 import {User, type IUser} from '../models/User.js'
 
 router.post('/register', UserController.register)
+router.post('/login', UserController.login)
