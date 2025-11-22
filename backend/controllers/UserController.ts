5c5c88a6c6e7d07ba9670e37c2c93ac6e95318d2
JulianoMXM <juliano.massocalegal@gmail.com>
2025-11-22 01:32:25 -0300

    feat: Adiciona o login do usuário com validação de email e senha

--- backend/controllers/UserController.ts
+++ backend/controllers/UserController.ts
@@ -72,4 +72,37 @@ export class UserController {
 
     }
 
+    static async login(req: Request, res: Response){
+
+        const {email, password} = req.body
+
+        if(!email){
+            res.status(422).json({message: 'O email é obrigatório.'})
+            return
+        }
+        if(!password){
+            res.status(422).json({message: 'A senha é obrigatória.'})
+            return
+        }
+
+        const user = await User.findOne({email: email})
+
+        //Checa se o email existe
+
+        if(!user){
+            res.status(422).json({message: 'Email inválido.'})
+            return
+        }
+
+        const checkPassword = await bcrypt.compare(password, user.password)
+
+        if(!checkPassword){
+            res.status(422).json({message: 'Senha inválida.'})
+            return
+        }
+
+        await createUserToken(user, req, res)
+
+    }
+
 }
