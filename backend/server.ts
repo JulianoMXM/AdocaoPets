20ac348e40460b5701dfe57caee36310b6b43585
JulianoMXM <juliano.massocalegal@gmail.com>
2025-11-22 01:32:06 -0300

    feat: Importa as rotas de pet e usuário
    refact: Comenta o uso das rotas de pets não implementadas

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config'
 import express from 'express'
 import { main } from './db/conn'
+import { router as UserRoutes} from './routes/UserRoutes'
+import { router as PetRoutes} from './routes/PetRoutes'
 const app = express()
 
 //  Configuração de leitura de JSON
@@ -24,36 +26,4 @@ main().then(() => {
 //  Rotas da API
 
 app.use('/users', UserRoutes)
-app.use('/pets', PetRoutes)
-import 'dotenv/config';
-import express from 'express';
-import mongoose from 'mongoose';
-const app = express();
-
-//  Configuração de leitura de JSON
-
-app.use(
-    express.urlencoded({
-        extended: true
-    })
-);
-
-app.use(express.json());
-
-//  Porta utilizada
-
-const DB_USER = process.env.DB_USER
-const DB_PASSWORD = process.env.DB_PASSWORD
-
-mongoose
-    .connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@projetosunect.wtzr5ke.mongodb.net/`
-)
-    .then(() => {
-        console.log('Conectado');
-        app.listen(3000);
-    })
-    .catch((error) => {
-        console.error('Erro ao conectar ao banco:', error);
-    })
-    
-//  Rotas da API
+//app.use('/pets', PetRoutes)
