1ec3c4c5abb5464797426059c695c40664ae944e
JulianoMXM <juliano.massocalegal@gmail.com>
2025-11-24 12:38:11 -0300

    refact: Ajusta server.ts para estar dentro da rootDir e ajusta as importações

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,8 @@
 import 'dotenv/config'
 import express from 'express'
-import { main } from './db/conn'
-import { router as UserRoutes} from './routes/UserRoutes'
-import { router as PetRoutes} from './routes/PetRoutes'
+import { main } from './db/conn.js'
+import { router as UserRoutes} from './routes/UserRoutes.js'
+import { router as PetRoutes} from './routes/PetRoutes.js'
 const app = express()
 
 //  Configuração de leitura de JSON
