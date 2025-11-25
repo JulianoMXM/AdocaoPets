d441494088af46862acdab7de36ebb7b22c9eefc
JulianoMXM <juliano.massocalegal@gmail.com>
2025-11-25 13:08:18 -0300

    feat: Adiciona as novas rotas e verificação de token

--- backend/src/routes/UserRoutes.ts
+++ backend/src/routes/UserRoutes.ts
@@ -4,8 +4,12 @@ export const router = Router()
 import type {Response, Request } from 'express'
 import {User, type IUser} from '../models/User.js'
 
+//Token
+import { checkToken } from '../helpers/verify-token.js'
+
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 router.get('/checkUser', UserController.checkUser)
 router.get('/:id', UserController.findUserById)
-router.patch('/edit/:id', UserController.editUser)
+router.patch('/:id', checkToken, UserController.editUser)
+router.delete('/:id', checkToken, UserController.deleteUser)
